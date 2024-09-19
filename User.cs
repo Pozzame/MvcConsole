a40a43bb295f22b88769966cd007e5fb3b4ec387
Pozzame <pozzame@gmail.com>
2024-09-19 17:59:32 +0200

    w

--- User.cs
+++ User.cs
@@ -1,15 +1,15 @@
 class User{
-    private String name;
+    private string name;
     private int id;
 
     public string Name { get => name; set => name = value; }
     public int Id { get => id; set => id = value; }
 
-    public User(String name, int id) {
+    public User(string name, int id) {
         Name = name;
         Id = id;
     }
-    public override String ToString(){
+    public override string ToString(){
         return $"{Id}\t{Name}";
     }
 }
